8c985c721df76f0b14e8d1e1e5914588b4b8788c
f1kusss <93314379+f1kusss@users.noreply.github.com>
2021-11-03 01:37:40 +0300

    Add files via upload

---
+++ Нахождение факториала/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Нахождение_факториала
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p, factorial, b;
+            factorial = 1;
+            Console.Write("Факториал какого числа вам надо посчитать? ");
+            p = Convert.ToInt32(Console.ReadLine());
+            b = p;
+            if (p >= 1)
+            {
+                do
+                {
+                    factorial *= p;
+                    p--;
+                }
+                while (p > 0);
+                Console.WriteLine($"{b}!={factorial}");
+            }
+            else if (p == 0)
+            {
+                Console.WriteLine("0!=1");
+            }
+            else
+            {
+                Console.WriteLine("Невозможно посчитать данный факториал");
+            }
+            
+            
+        }
+    }
+}
---
+++ Парные числа/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Парные_числа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, sum, schet;
+            sum = 0;
+            schet = 0;
+            Console.Write("Введите до какого числа нужно посчитать парные числа: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    schet++;
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"Количество парных чисел:{schet}");
+            Console.WriteLine($"Сумма этих чисел:{sum}");
+        }
+    }
+}
