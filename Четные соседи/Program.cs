dd754eea8025c049d10b51c64c47a329f7d38e38
f1kusss <93314379+f1kusss@users.noreply.github.com>
2022-02-28 17:55:01 +0300

    Add files via upload

---
+++ Четные соседи/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Четные_соседи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, counter=0;
+
+            n = int.Parse(Console.ReadLine());
+            int[] array;
+            array = new int[n+2];
+            array[0] = 0;
+            array[n + 1] = 0;
+            for (i = 1; i <= n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (i = 1; i <= n; i++)
+            {
+                if (array[i - 1] % 2 == 0 & array[i + 1] % 2 == 0)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"Кол-во данных элементов {counter}");
+        }
+    }
+}
